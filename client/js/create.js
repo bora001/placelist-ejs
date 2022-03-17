3f043ecf6117dd6a2e44d0a97780e03c4f913107
bora001 <bora.vancouver@gmail.com>
2022-03-17 17:14:25 +0900

    edit : create place

--- client/js/create.js
+++ client/js/create.js
@@ -1,32 +1,3 @@
-const createForm = async () => {
-  console.log("createForm!-client");
-  // const submitBtn = document.querySelector(".btn_submit");
-  // const form = document.querySelector(".form_new");
-  // const formData = new FormData(form);
-  // form.addEventListener("submit", (e) => {
-  //   const str = "Upload.......";
-  //   submitBtn.disable = true;
-  //   const count = (word, index) => {
-  //     setTimeout(() => {
-  //       submitBtn.value = word;
-  //     }, 150 * index);
-  //   };
-  //   for (let i in str) {
-  //     let s = str.slice(0, Number(i) + 1);
-  //     count(s, i);
-  //   }
-  // });
-  // const res = await fetch("/create", {
-  //   method: "POST",
-  //   body: formData,
-  // });
-  // const data = await res.json();
-  // if (data.success) {
-  //   formReset();
-  //   window.location.href = "/";
-  // }
-};
-
 const thumbnail = () => {
   const fileBtn = document.getElementById("input_img");
   const preImg = document.querySelector(".section_new .pre_img");
--- middleware.js
+++ middleware.js
@@ -3,7 +3,7 @@ module.exports.authCheck = (req, res, next) => {
   console.log(req.url, "url.req.");
   if (!req.user) {
     console.log("login first!");
-    req.flash("pass", "Please login");
+    req.flash("pass", "Please Login first");
     req.session.returnTo = req.originalUrl;
     res.redirect("/login");
     return;
